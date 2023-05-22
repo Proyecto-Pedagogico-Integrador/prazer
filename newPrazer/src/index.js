b8fb77749a0d33e5a10ced9b8872a810527c50ee
JorgeACardona <andres20cardona@gmail.com>
2023-05-22 14:09:23 -0500

    Rediseño tablas

--- newPrazer/src/index.js
+++ newPrazer/src/index.js
@@ -56,6 +56,7 @@ app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
 app.use('/links', require('./routes/links'));
 app.use('/producto', require('./routes/producto'));
+app.use('/cliente', require('./routes/cliente'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ newPrazer/src/routes/cliente.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
+
+router.get('/add', (req, res) => {
+    res.render('cliente/add');
+});
+
+router.post("/add", async (req, res) => {
+    try {
+        const { nombre, nit, telefono, direccion } = req.body
+        const newCliente = {
+            nombre, 
+            nit, 
+            telefono, 
+            direccion
+        }
+        await pool.query('INSERT INTO cliente set ?',[newCliente]);
+        req.flash('success', 'Cliente guardado exitosamente');
+        res.redirect('/cliente');
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const cliente = await pool.query('SELECT * FROM cliente');
+    res.render('cliente/list', { cliente });
+});
+
+router.get('/delete/:id_cliente', async (req, res) => {
+    const { id_cliente } = req.params;
+    await pool.query('DELETE FROM cliente WHERE id_cliente = ?', [id_cliente]);
+    req.flash('success', 'Cliente eliminado exitosamente');
+    res.redirect('/Cliente');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const cliente = await pool.query('SELECT * FROM cliente WHERE id_cliente = ?', [id]);
+    console.log(cliente);
+    res.render('cliente/edit', {cliente: cliente[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, nit, telefono, direccion } = req.body
+    const newCliente = {
+        nombre, 
+        nit, 
+        telefono, 
+        direccion
+    }
+    await pool.query('UPDATE cliente set ? WHERE id_cliente = ?', [newCliente, id]);
+    req.flash('success', 'Cliente actualizado exitosamente');
+    res.redirect('/Cliente');
+});
+
+module.exports = router;
