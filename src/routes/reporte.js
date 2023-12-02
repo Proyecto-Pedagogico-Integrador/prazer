bdf4163ed9340becd5bddd581248fdc3120b2b4d
idunnowhydamr <diego_munoz@franciscomiranda.edu.co>
2023-12-01 21:54:47 -0500

    se hace el back de reportes

--- src/routes/reporte.js
+++ src/routes/reporte.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('reporte/listReporte');
+const pool = require("../database");
+const { isLoggedIn } = require("../lib/auth");
+
+router.get('/', async (req, res) => {
+  try {
+    const cantidadMaxima = await pool.query(
+      `SELECT 
+        nombre, 
+        cantidad as atributo,
+        'cantidad maxima' as tipo 
+      FROM producto 
+      order by cantidad desc 
+      limit 1`);
+    
+    const cantidadMinima = await pool.query(
+      `SELECT 
+        nombre, 
+        cantidad as atributo, 
+        'cantidad minima' as tipo 
+      FROM producto 
+      order by cantidad asc 
+      limit 1`);
+
+    const precioMayor = await pool.query(
+      `SELECT 
+        nombre, 
+        precio as atributo, 
+        'precio mas alto' as tipo 
+      FROM producto 
+      order by precio desc 
+      limit 1`);
+
+    const precioMenor = await pool.query(
+      `SELECT 
+        nombre, 
+        precio as atributo,
+        'precio mas bajo' as tipo 
+      FROM producto 
+      order by precio asc 
+      limit 1
+      `)
+
+    res.render('reporte/listReporte',{ cantidadMaxima, cantidadMinima, precioMayor, precioMenor});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
+    
   });
 
 module.exports = router;
