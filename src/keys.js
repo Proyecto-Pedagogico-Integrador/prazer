4cba5f9891566824895658c0903be94202a4f756
idunnowhydamr <diego_munoz@franciscomiranda.edu.co>
2024-05-31 12:37:43 -0500

    config database

--- src/keys.js
+++ src/keys.js
@@ -1,15 +1,15 @@
 module.exports = {
 
-    // database: {
-    //     host:'bnagucyl18kcsrgm8ym0-mysql.services.clever-cloud.com',
-    //     database:'bnagucyl18kcsrgm8ym0',
-    //     user:'uatj3gfvz68ohkny',
-    //     password: 'QxbMhLs2fNC64IuDMabu'
-    // }
     database: {
-        connectionLimit: 15,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'prazerDB'}
+        host:'bnagucyl18kcsrgm8ym0-mysql.services.clever-cloud.com',
+        database:'bnagucyl18kcsrgm8ym0',
+        user:'uatj3gfvz68ohkny',
+        password: 'QxbMhLs2fNC64IuDMabu'
+    }
+    // database: {
+    //     connectionLimit: 15,
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'prazerDB'}
 };
