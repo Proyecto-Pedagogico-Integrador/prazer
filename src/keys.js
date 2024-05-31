b7140198d9fa056bab09477d5e5f1009fe2c6ca9
idunnowhydamr <diego_munoz@franciscomiranda.edu.co>
2024-05-31 13:40:32 -0500

    config database paga x2

--- src/keys.js
+++ src/keys.js
@@ -1,21 +1,20 @@
 module.exports = {
-
-    // database: {
-    //     host:'bnagucyl18kcsrgm8ym0-mysql.services.clever-cloud.com',
-    //     database:'bnagucyl18kcsrgm8ym0',
-    //     user:'uatj3gfvz68ohkny',
-    //     password: 'QxbMhLs2fNC64IuDMabu'
-    // }
-    database: {
-        host:'bfl7tkeifiu6o3cbacsq-mysql.services.clever-cloud.com',
-        database:'bfl7tkeifiu6o3cbacsq',
-        user:'uprcfc3jezpdr97p',
-        password: 'CmibzVkVRK5BLSMesaP'
-    }
-    // database: {
-    //     connectionLimit: 15,
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: '',
-    //     database: 'prazerDB'}
+  // database: {
+  //     host:'bnagucyl18kcsrgm8ym0-mysql.services.clever-cloud.com',
+  //     database:'bnagucyl18kcsrgm8ym0',
+  //     user:'uatj3gfvz68ohkny',
+  //     password: 'QxbMhLs2fNC64IuDMabu'
+  // }
+  database: {
+    host: "bfl7tkeifiu6o3cbacsq-mysql.services.clever-cloud.com",
+    database: "bfl7tkeifiu6o3cbacsq",
+    user: "uprcfc3jezpdr97p",
+    password: "CmibzVkVRK5BLSMesaP",
+  }
+  // database: {
+  //     connectionLimit: 15,
+  //     host: 'localhost',
+  //     user: 'root',
+  //     password: '',
+  //     database: 'prazerDB'}
 };
